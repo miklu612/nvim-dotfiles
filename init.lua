bfd3a69433f3fe3dc0670aa72a7a69eb6fce6878
Michael <robo.dude@mail.com>
2023-12-17 16:14:23 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,54 @@
+
+-- Installing lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Plugins
+require("lazy").setup({
+    {
+        "folke/tokyonight.nvim",
+        init = function()
+            vim.cmd.colorscheme("tokyonight")
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("lspconfig").clangd.setup({})
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            highlight = {
+                enable = true
+            }
+        },
+        config = function(_, opts) 
+            require("nvim-treesitter.configs").setup(opts)
+	end
+    },
+})
+
+-- My options
+vim.opt.expandtab  = true
+vim.opt.shiftwidth = 4
+vim.opt.number     = true
+vim.opt.hlsearch   = false
+vim.opt.mouse      = ""
+vim.opt.textwidth  = 79
+vim.g.mapleader    = " "
+
+-- Keybindings
+vim.keymap.set("n", "<leader>op", function() vim.cmd(":Explore") end)
