b940e1c34bae19c02a0e8759ce0e9380d34760a0
Michael <robo.dude@mail.com>
2024-08-20 13:03:53 +0300

    Updated lsp

--- init.lua
+++ init.lua
@@ -62,17 +62,29 @@ require("lazy").setup({
         opts = {},
         enabled = true,
         config = function(_, opts) 
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local bufnr = args.buf
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    vim.bo[bufnr].completefunc = "v:lua.vim.lsp.omnifunc"
+                    vim.api.nvim_set_keymap("i", "<C-K>", "", {
+                        callback = function()
+                            vim.complete()
+                        end
+                    })
+                end
+            })
             require("lspconfig").rust_analyzer.setup({
                 settings = {
                     ['rust-analyzer'] = {}
-                }
+                },
             })
             require("lspconfig").clangd.setup({
                 settings = {
                     cmd = {
                         "clangd",
                     }
-                }
+                },
             })
         end
     },
@@ -87,16 +99,17 @@ require("lazy").setup({
 
 
 -- My options
-vim.opt.expandtab  = true
-vim.opt.shiftwidth = 4
-vim.opt.number     = true
-vim.opt.hlsearch   = false
-vim.opt.mouse      = ""
-vim.opt.textwidth  = 79
-vim.opt.signcolumn = "yes:1"
-vim.opt.cursorline = true
-vim.g.mapleader    = " "
-vim.g.netrw_banner = 0
+vim.opt.expandtab   = true
+vim.opt.shiftwidth  = 4
+vim.opt.number      = true
+vim.opt.hlsearch    = false
+vim.opt.mouse       = ""
+vim.opt.textwidth   = 79
+vim.opt.signcolumn  = "yes:1"
+vim.opt.cursorline  = true
+vim.opt.completeopt = "menu"
+vim.g.mapleader     = " "
+vim.g.netrw_banner  = 0
 
 vim.cmd.colorscheme("tokyonight-night")
 
