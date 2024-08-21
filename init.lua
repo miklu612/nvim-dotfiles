b52b7a8fc6b085ba6d945577c614c74eab8f8ac3
Michael <robo.dude@mail.com>
2024-08-21 12:11:37 +0300

    Added a lua lsp

--- init.lua
+++ init.lua
@@ -86,6 +86,24 @@ require("lazy").setup({
                     }
                 },
             })
+            require("lspconfig").lua_ls.setup({
+                on_init = function(client)
+                    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                        runtime = {
+                            version = "LuaJIT"
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME
+                            }
+                        }
+                    })
+                end,
+                settings = {
+                    Lua = {}
+                }
+            })
         end
     },
     {
@@ -94,7 +112,7 @@ require("lazy").setup({
         dependencies = {
             'nvim-lua/plenary.nvim'
         }
-    }
+    },
 })
 
 
