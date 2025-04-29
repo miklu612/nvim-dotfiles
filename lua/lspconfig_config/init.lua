7ebfbc0159a87a93caed0952a8f0c029a83229ea
Monika <>
2025-04-29 12:50:28 +0300

    feat: Janky way to get line diagnostics

--- lua/lspconfig_config/init.lua
+++ lua/lspconfig_config/init.lua
@@ -10,6 +10,11 @@ return {
                 vim.lsp.inlay_hint.enable(true)
                 vim.lsp.completion.enable(true, args.data.client_id, bufnr)
                 vim.bo[bufnr].completefunc = "v:lua.vim.lsp.omnifunc"
+
+                vim.keymap.set("n", "<Leader>od", function()
+                    vim.print(vim.diagnostic.get())
+                end)
+
                 vim.api.nvim_create_autocmd("InsertCharPre", {
                     buffer = vim.api.nvim_get_current_buf(),
                     callback = function()
